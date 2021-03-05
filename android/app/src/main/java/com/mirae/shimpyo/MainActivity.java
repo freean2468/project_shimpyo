54e5f132caeb0b0311cb8fa5e3a5df83a174baef
sensebecommon <freean2468@gmail.com>
2021-03-05 10:58:12 +0900

    #jerry_test_1
    주석으로 차이를 알아보자

--- android/app/src/main/java/com/mirae/shimpyo/MainActivity.java
+++ android/app/src/main/java/com/mirae/shimpyo/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 주석을 달아서 내용을 추가하면면
     }
-}
