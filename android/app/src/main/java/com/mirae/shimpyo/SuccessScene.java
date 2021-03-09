b506b55a6be6c02fe8b05b9fa2d0b49460902b13
TaekminKim <tackmin080@gmail.com>
2021-03-09 12:56:34 +0900

    #android_kakao login 기능구현

---
+++ android/app/src/main/java/com/mirae/shimpyo/SuccessScene.java
@@ -0,0 +1,13 @@
+package com.mirae.shimpyo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SuccessScene extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+
+    }
+}
